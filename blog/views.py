04ec29c1ac75d75ed70e5fa3b6cfc27441f1e820
Lucia Pane <lucia.r.pane@gmail.com>
2016-09-09 14:02:27 -0500

    adding template files for all views

--- blog/views.py
+++ blog/views.py
@@ -2,18 +2,15 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
 def home(request):
-    pass
+    return render(request, 'blog/index.html')
 
 def about(request):
     pass
 
 def podcast(request):
-    pass
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/podcast.html', {'posts': posts})
 
 def gallery(request):
     pass
