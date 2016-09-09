844cde1b5e8552dffd93f1a14edc720c3e347341
Lucia Pane <lucia.r.pane@gmail.com>
2016-09-09 11:51:20 -0500

    adding urls and views for all pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^//$', views.home , name='home'),
+    url(r'^/podcast/$', views.podcast, name='podcast'),
+    url(r'^/gallery/$', views.gallery , name='gallery'),
+    url(r'^/orderform/$', views.order_form , name='orderform'),
+    url(r'^/about/$', views.about , name='about'),
+    url(r'^/contact/$', views.contact , name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,21 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def home(request):
+    pass
+
+def about(request):
+    pass
+
+def podcast(request):
+    pass
+
+def gallery(request):
+    pass
+
+def orderform(request):
+    pass
+
+def contact(request):
+    pass
