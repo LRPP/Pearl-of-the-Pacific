43d2fa5864a2ef41f325c25aee02c9b6447c1af6
Lucia Pane <lucia.r.pane@gmail.com>
2016-09-15 14:47:25 -0500

    adding episode view

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home , name='home'),
     url(r'^podcast$', views.podcast, name='podcast'),
+    url(r'^post/(?P<pk>\d+)/$', views.episode, name='episode'),
     url(r'^gallery$', views.gallery , name='gallery'),
     url(r'^orderform$', views.orderform , name='orderform'),
     url(r'^about$', views.about , name='about'),
--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,10 @@ def podcast(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/podcast.html', {'posts': posts})
 
+def episode(request):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/episode.html', {'post': post})
+
 def gallery(request):
     return render(request, 'blog/gallery.html')
 
